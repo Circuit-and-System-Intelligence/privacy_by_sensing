4a22db52049b2b07cc1d0559e2d06e61274200f7
Jianbo Liu <jliu32@nd.edu>
2022-06-16 11:22:53 -0400

    Add random projection and MNIST data

--- src/svm_flatten.py
+++ src/svm_flatten.py
@@ -9,7 +9,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 import numpy as np
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 
 
 def nonlinearity(x, mode=0):
@@ -53,8 +54,8 @@ def MNIST_DATASET_TRAIN(train_amount, downloads=False):
     train_labels = training_data.targets.numpy()[:train_amount]
     print("Training data size: ", train_data.shape)
     print("Training data label size: ", train_labels.shape)
-    plt.imshow(train_data[0])
-    plt.show()
+    # plt.imshow(train_data[0])
+    # plt.show()
 
     train_data = train_data / 255.0
 
@@ -71,124 +72,42 @@ def MNIST_DATASET_TEST(test_amount, downloads=False):
     test_labels = testing_data.targets.numpy()[:test_amount]
     print("Testing data size: ", test_data.shape)
     print("Testing data label size: ", test_labels.shape)
-    plt.imshow(test_data[0])
-    plt.show()
 
     test_data = test_data / 255.0
 
     return test_data, test_labels
 
 
-def train(args, model, device, train_loader, optimizer, epoch, RP):
-    # avg_loss_epoch = 0
-    batch_loss = 0
-    total_batches = 0
-    l2error = []
-    sigma_sum = []
-    for i, (images, labels) in enumerate(train_loader):
-        # Reshape images to (batch_size, input_size)
-        images = images.reshape(-1, 28 * 28)
-        dista = nn.functional.pdist(images)
-        dista = dista[dista.nonzero()]
-        a1 = dista.mean() ** 2
-        sigma = (l2 / dista.mean()) * np.sqrt(np.log(1 / delta)) / epsilon
-        sigma_sum += [sigma]
-        # if i == 0:
-        # sns.displot(image.flatten(), bins=16)
-        # plt.savefig("image-hist.png")
-        # sum = images[0].matmul(torch.abs(RP)).sum()
-        images = images.matmul(RP)
-        distb = nn.functional.pdist(images)
-        distb = distb[distb.nonzero()]
-        a2 = distb.mean() ** 2
-        l2error += [((a1 - a2) ** 2) / (a1 ** 2)]
-        images = images.reshape(-1, k)
+# Train the model
+def train(model, train_data, train_label, args):
+    model.fit(train_data, train_label)
+    score = model.score(train_data, train_label)
+    print("Score: ", score)
 
-        # Forward pass
-        outputs = svm_model(images)
-        loss_svm = svm_loss_criteria(outputs, labels)
+    cv_scores = cross_val_score(model, train_features, train_label, cv=10)
+    print("CV average score: %.2f" % cv_scores.mean())
+    return model
 
-        # Backward and optimize
-        svm_optimizer.zero_grad()
-        loss_svm.backward()
-        svm_optimizer.step()
 
-        # print("Model's parameter after the update:")
-        # for param2 in svm_model.parameters():
-        #   print(param2)
-        total_batches += 1
-        batch_loss += loss_svm.item()
-
-    avg_loss_epoch = batch_loss / total_batches
-    l2error = np.mean(l2error)
-    sigma_sum = np.mean(sigma_sum)
-    print(
-        "Epoch [{}/{}], Averge Loss:for epoch[{}, {:.4f}]".format(
-            epoch + 1, num_epochs, epoch + 1, avg_loss_epoch
-        )
+def generate_projection_matrix(k, args):
+    random_projection_matrix = np.random.normal(0, 1 / np.sqrt(k), size=(28 * 28, k))
+    random_projection_matrix = (
+        np.digitize(random_projection_matrix, np.linspace(-1, 1, 32)) - 16
     )
+    if args.nonlinear_mult:
+        random_projection_matrix = nonlinearity(random_projection_matrix, mode=1)
+
+    l2 = random_projection_matrix ** 2
+
+    l2 = np.sqrt(l2.sum(1))
+
+    print("L2 distance", l2.max())
+
+    return random_projection_matrix
 
 
-def main(k, epsilon):
-    batch_size = 256
-    num_classes = 10
-    learning_rate = 0.01
-
-    RP = torch.tensor(
-        # gs.gram_schmidt(np.random.normal(0, 1 / k, size=(28 * 28, k)))[0],
-        np.random.normal(0, 1 / np.sqrt(k), size=(28 * 28, k)),
-        dtype=torch.float,
-    )
-    RP = torch.bucketize(RP, torch.linspace(-1, 1, steps=32)) - 16
-    # RP = RP / 16.0
-    RP = nonlinearity(RP, mode=1)
-
-    l2 = RP ** 2
-
-    l2 = l2.sum(1).sqrt().max()
-
-    delta = 0.1
-    # SVM regression model and Loss
-    svm_model = nn.Linear(k, num_classes)
-    # model = LogisticRegression(input_size,num_classes)
-
-    # # Loss criteria and SGD optimizer
-    # svm_loss_criteria = nn.MultiLabelMarginLoss()
-    svm_loss_criteria = nn.CrossEntropyLoss()
-
-    svm_optimizer = torch.optim.Adam(svm_model.parameters(), lr=learning_rate)
-
-    print("l2error", l2error)
-    print("sigma", sigma_sum)
-    result = [k, epsilon]
-    result += [l2error, sigma_sum]
-    # Test the SVM Model
-    correct = 0.0
-    total = 0.0
-    for images, labels in test_loader:
-        images = images.reshape(-1, 28 * 28)
-        dist = nn.functional.pdist(images)
-        dist = dist[dist.nonzero()]
-        images = images.matmul(RP)
-        sigma = (l2 / dist.mean()) * np.sqrt(np.log(1 / delta)) / epsilon
-        AN = torch.tensor(
-            np.random.normal(0, sigma, size=(len(images), k)), dtype=torch.float
-        )
-        images = images.add(AN)
-        images = images.reshape(-1, k)
-
-        outputs = svm_model(images)
-        predicted = outputs.data.max(1, keepdim=True)[1]
-        total += labels.size(0)
-        correct += (predicted.view(-1).long() == labels).sum()
-
-    accuracy = 100 * (correct.float() / total)
-    print(f"{accuracy:.2f}")
-    result += [accuracy]
-    with open("svm_full.csv", "ab") as f:
-        np.savetxt(f, result, newline=", ", fmt="%.2f")
-        f.write(b"\n")
-    return result
+def generate_additive_noise(k, sigma, size, args):
+    return np.random.normal(0, sigma, size=(size, k))
 
 
 if __name__ == "__main__":
@@ -205,6 +124,17 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--test_amount", type=int, default=2000, help="Amount of testing samples"
+    )
+    parser.add_argument(
+        "--projection_dimension",
+        type=int,
+        default=80,
+        help="Dimensions after projection",
+    )
+    parser.add_argument(
+        "--nonlinear_mult",
+        default=False,
+        help="Add nonlinear multiplication effects",
     )
     args = parser.parse_args()
 
@@ -224,20 +154,17 @@ if __name__ == "__main__":
     train_features = train_data.reshape(args.train_amount, -1)
     test_features = test_data.reshape(args.test_amount, -1)
 
-    print("Training features", train_features.shape)
+    rnd_matrix = generate_projection_matrix(args.projection_dimension, args)
+    train_features = np.matmul(train_features, rnd_matrix)
+    test_features = np.matmul(test_features, rnd_matrix)
 
     # Train SVM
     print("-----Training SVM-----")
     lsvc = LinearSVC(dual=False)
-    print(lsvc)
 
-    lsvc.fit(train_features, train_label)
-    score = lsvc.score(train_features, train_label)
-    print("Score: ", score)
+    lsvc = train(lsvc, train_features, train_label, args)
 
-    cv_scores = cross_val_score(lsvc, train_features, train_label, cv=10)
-    print("CV average score: %.2f" % cv_scores.mean())
-
+    print("-----Testing SVM------")
     pred_label = lsvc.predict(test_features)
 
     cm = confusion_matrix(test_label, pred_label)
@@ -245,11 +172,3 @@ if __name__ == "__main__":
 
     cr = classification_report(test_label, pred_label)
     print(cr)
-
-# results = []
-# for k in [40, 60, 80, 100, 120, 160, 180]:
-#     for e in [0.2, 0.4, 0.6, 0.8, 1]:
-#         for i in range(200):
-#             print(e, i)
-#             measure = main(k, e)
-#             results.append([k, e, measure])
